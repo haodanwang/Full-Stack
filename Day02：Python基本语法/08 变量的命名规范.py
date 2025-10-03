c0129cbc099f2acb2a8e4b59d607037c70d24984
shanks <939565638@qq.com>
2025-10-03 23:29:10 +0800

    10.3学习

---
+++ Day02：Python基本语法/08 变量的命名规范.py
@@ -0,0 +1,16 @@
+# (1) 规范 见名知意
+
+username = "shanks"
+age = 18
+# (2) 变量名应该遵循一定的命名约定
+# 驼峰命名法 或下划线命名法
+# 驼峰命名法小驼峰首字母不大写 后续都大写 建议4个单词左右
+# playerAttackValue = 100
+# # 下划线命名法
+# player_attack_value = 100
+
+# （3）规则 变量必须是字母数字以及下划线的组合
+# 不能有下划线以外的特殊符号
+# 不能以数字开头
+# （4）变量名不能使用关键字（保留字 内置函数名）
+# 关键字：False,None,True,and,as,assert,async,await,break,class,continue,def,del,elif,else,except,finally,for,from,global,if,import,in,is,lambda,nonlocal,not,or,pass,raise,return,try,while,with,yield
