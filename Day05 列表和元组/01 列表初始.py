709baeddd4dd46a213484b1c4bbc45e0f9244465
shanks <939565638@qq.com>
2025-10-06 23:12:55 +0800

    10.6学习内容

---
+++ Day05 列表和元组/01 列表初始.py
@@ -0,0 +1,7 @@
+gf_name_list = ["高圆圆", "刘亦菲", "赵丽颖", "范冰冰", "李嘉欣"]
+
+# print(type(gf_name_list))
+# print(gf_name_list[0])
+for i in gf_name_list:
+    if i.startswith("赵"):
+        print(i)
---
+++ Day05 列表和元组/02 列表的序列操作.py
@@ -0,0 +1,15 @@
+gf_name_list = ["高圆圆", "刘亦菲", "赵丽颖", "范冰冰", "李嘉欣"]
+
+# 索引 列表对象[索引]，获取元素
+# print(gf_name_list[-1])
+
+
+# 切片操作 列表对象[开始索引：结束索引]
+print(gf_name_list[1:4])
+print(gf_name_list[::2])
+print(gf_name_list[::-1])
+
+# 修改元素 列表对象[索引]=值
+
+gf_name_list[3] = "佟丽娅"
+print(gf_name_list)
