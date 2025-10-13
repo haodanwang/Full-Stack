d2d72c718bfc9d3c9e127cf08f4ac2a57655a374
shanks <939565638@qq.com>
2025-10-13 23:55:49 +0800

    模块2 函数

---
+++ 模块二/Day07/01 函数初识.py
@@ -0,0 +1,23 @@
+# 函数版本
+
+# 函数声明
+"""
+def 函数名():
+    函数体
+"""
+
+
+def print_pokes():
+    poke_type = ["♥️", "♦️", "♠️", "♣️"]
+    poke_num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    poke_list = []
+    for type in poke_type:
+        for num in poke_num:
+            poke_list.append(f"{type}{num}")
+    print(poke_list)
+
+
+# 函数调用 一次声明 多次调用
+print_pokes()
+print("somethings")
+print_pokes()
---
+++ 模块二/Day07/02 函数参数概述.py
@@ -0,0 +1,12 @@
+# 1 计算1-100的累加和
+
+
+def cal():
+    ret = 0
+    for i in range(1, 101):
+        ret += i
+
+    print(ret)
+
+
+cal()
